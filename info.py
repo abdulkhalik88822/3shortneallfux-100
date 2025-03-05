15c334104ed74ea927a85df8623f1edc47f29b16
abdulkhalik88822 <125821386+abdulkhalik88822@users.noreply.github.com>
2025-03-05 13:32:39 +0530

    Update info.py

--- info.py
+++ info.py
@@ -56,7 +56,7 @@ THREE_VERIFY_GAP = int(environ.get('THREE_VERIFY_GAP', "600"))
 # languages search
 LANGUAGES = ["hindi", "english", "telugu", "tamil", "kannada", "malayalam"]
 
-auth_channel = environ.get('AUTH_CHANNEL', '-1001836465539')
+auth_channel = environ.get('AUTH_CHANNEL', '-1001954276464')
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
 SUPPORT_GROUP = int(environ.get('SUPPORT_GROUP', '-1001626107740'))
 
